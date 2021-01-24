cf4e606604baa2f81feb395362400f9ef8398a60
wuda20 <daniel.wu35@qmail.cuny.edu>
2021-01-23 23:52:49 -0500

    switched to Vue/Vite

---
+++ vt/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <Form />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Form from './components/Form.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Form
+  }
+}
+</script>
---
+++ vt/src/components/Form.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id='par'>
+    <h2>Create Your Account</h2>
+    <form id='form'>
+        <input class="form__input" type='text' placeholder='Email'/>
+        <input class="form__input" type='text' placeholder='Username'/>
+        <input class="form__input" type='password' placeholder='Password'/>
+        <input class="form__input" type='password' placeholder='Confirm Password'/>
+        <div class='form__submit' @click='submitForm'>
+        Submit
+        </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+  },
+  validations: {
+  },
+  methods: {
+    submitForm () {
+      alert('Form has been submitted')
+    }
+  }
+}
+</script>
+
+<style scoped>
+#form {
+    margin: 0 auto;
+}
+#par {
+display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.form__input {
+  border: none;
+  outline: none;
+  border-bottom: 1px solid #eee;
+  font-size: 1em;
+  padding: 5px;
+  display: flex;
+  margin: 10px 0 5px 0;
+}
+
+.form__error {
+  color: red;
+  font-size: 0.75em;
+  padding-left: 10px;
+}
+
+.form__submit {
+  background-color: #0033cc;
+  color: white;
+  padding: 10px 20px;
+  text-align: center;
+  cursor: pointer;
+  margin-top: 30px;
+}
+</style>
